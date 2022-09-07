6eea32ef46a2feebe187b2cf20b536468628ac7c
EmineKayhan <105215871+EmineKayhan@users.noreply.github.com>
2022-09-07 12:21:19 +0300

    soru03

---
+++ soru03.java
@@ -0,0 +1,17 @@
+package sorular;
+
+public class soru03 {
+
+	public static void main(String[] args) {
+		// 1'den 100'e kadar tek sayýlarý yazdýran program
+		
+		for(int i=0;i<100;i++){
+			if(i%2==1) {
+				System.out.println(i);
+			}
+		}
+		
+
+	}
+
+}
