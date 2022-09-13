5425332c4a3336703cb592d243595437d609a444
EmineKayhan <105215871+EmineKayhan@users.noreply.github.com>
2022-09-13 23:36:58 +0300

    soru06

---
+++ soru06.java
@@ -0,0 +1,20 @@
+package sorular;
+
+import java.util.ArrayList;
+
+public class soru06 {
+
+	public static void main(String[] args) {
+		// tek dizi üzerinden dizi elemanlarýný tersten yazdýrma
+		
+		int[]dizi= {1,2,3,4,5,6,7,8,9};
+		int i;
+		
+		for(i=dizi.length;i>0;i--) {
+			System.out.print(i+"-");
+		}
+	
+
+	}
+
+}
